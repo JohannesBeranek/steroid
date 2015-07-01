3f7ea8bf4eea418e46aca641735b937bad46038b
Florian Kernler <kernler@super-fi.com>
2015-07-01 15:28:41 +0200

    copied modified files from live server

--- page/class.UHPage.php
+++ page/class.UHPage.php
@@ -44,16 +44,22 @@ class UHPage implements IURLHandler {
 		) );
 		*/
 		// JB 23.1.2014 temporary fix for no-primary-url-found bug till $filteredJoinData in RBStorage can be reenabled
-		$currentPagePrimaryData = $storage->selectFirst( 'RCPage', array( 
-			'fields' => array( 'primary' ), 
-			'where' => array( 'page:RCPageUrl.url', '=', '%1$s' ),
-			'vals' => array( $url ),
-			'name' => 'UHPage_currentPagePrimaryData'
+		$currentPagePrimaryData = $storage->selectFirst( 'RCPage', array(
+			'fields' => array( 'primary' ),
+			'where'  => array( 'page:RCPageUrl.url', '=', '%1$s' ),
+			'vals'   => array( $url ),
+			'name'   => 'UHPage_currentPagePrimaryData'
 		) );
 
-		$this->currentPage = RCPage::get( $storage, array( 'primary' => $currentPagePrimaryData['primary'] ), true );
+		$this->currentPage = RCPage::get( $storage, array( 'primary' => $currentPagePrimaryData[ 'primary' ] ), true );
 
-		$this->currentPage->load( array( '*', 'language.locale', 'language.iso639', 'template.filename', 'domainGroup.domainGroup:RCDomain.*'  ) );
+		$this->currentPage->load( array(
+			'*',
+			'language.locale',
+			'language.iso639',
+			'template.filename',
+			'domainGroup.domainGroup:RCDomain.*'
+		) );
 
 		// make sure filter criteria are loaded, otherwise we would get endless recursion
 		// potentially benefits performance as well by preventing fields to be loaded separately by lazy loading
@@ -74,7 +80,7 @@ class UHPage implements IURLHandler {
 		$locale = $this->currentPage->language->locale;
 
 		if ( $locale ) {
-			if ( !setlocale( LC_ALL, $locale ) ) {
+			if ( ! setlocale( LC_ALL, $locale ) ) {
 				// mac has different locales, so we try those as well, if setting locale fails
 				// (this is just to make testing locally on macs easier)
 				$ret = setlocale( LC_ALL, str_ireplace( 'utf8', 'UTF-8', $locale ) );
@@ -82,7 +88,7 @@ class UHPage implements IURLHandler {
 		}
 
 		$this->storageFilter = new RBStorageDataTypeValueFilter( array(
-			'DTSteroidLive' => $this->currentPage->live,
+			'DTSteroidLive'     => $this->currentPage->live,
 			'DTSteroidLanguage' => $this->currentPage->language->{Record::FIELDNAME_PRIMARY}
 		) );
 
@@ -92,6 +98,7 @@ class UHPage implements IURLHandler {
 			$handler = self::$classRequestHandler[ $classRequest ];
 
 			/* @var IClassRequestHandler $handler */
+
 			return $handler->handleClassRequest( $storage, $this->currentPage, $classRequest, $requestInfo );
 		}
 
@@ -99,17 +106,29 @@ class UHPage implements IURLHandler {
 		$templateRecord = $this->currentPage->template;
 
 
-		$context = array( 'page' => $this->currentPage, 'request' => $requestInfo, 'url' => $url, 'storage' => $storage );
+		$context = array(
+			'page'    => $this->currentPage,
+			'request' => $requestInfo,
+			'url'     => $url,
+			'storage' => $storage
+		);
 
 //		$areaJoins = $this->currentPage->{'page:RCPageArea'};
 		// saves some queries (about 4 per page area), and makes query cacheable
-		$areaJoins = $storage->selectRecords( 'RCPageArea', array( 'fields' => '*', 'where' => array( 'page', '=', '%1$s' ) ), NULL, NULL, NULL, array( $this->currentPage ), 'UHPage_pageArea' );
+		$areaJoins = $storage->selectRecords( 'RCPageArea', array(
+			'fields' => '*',
+			'where'  => array( 'page', '=', '%1$s' )
+		), null, null, null, array( $this->currentPage ), 'UHPage_pageArea' );
 
 		if ( ( $elementPrimary = intval( $requestInfo->getQueryParam( 'element' ) ) ) && ( $elementClass = $requestInfo->getQueryParam( 'class' ) ) ) { // AJAX request to an element
 			$elements = array();
 
 			foreach ( $areaJoins as $areaJoin ) {
-				$elements[ ] = new ArrayObject( array( 'element' => $areaJoin->area, 'columns' => $areaJoin->columns, 'key' => $areaJoin->{'key'} ), ArrayObject::ARRAY_AS_PROPS ); // TODO: more data?
+				$elements[ ] = new ArrayObject( array(
+					'element' => $areaJoin->area,
+					'columns' => $areaJoin->columns,
+					'key'     => $areaJoin->{'key'}
+				), ArrayObject::ARRAY_AS_PROPS ); // TODO: more data?
 			}
 
 			while ( $elJoin = array_shift( $elements ) ) {
@@ -127,7 +146,9 @@ class UHPage implements IURLHandler {
 					break;
 				}
 
-				if ( $elJoin instanceof IRecord && $elJoin->hidden ) continue; // skip hidden elements
+				if ( $elJoin instanceof IRecord && $elJoin->hidden ) {
+					continue;
+				} // skip hidden elements
 
 				$children = $elJoin->element->getChildren();
 
@@ -137,14 +158,22 @@ class UHPage implements IURLHandler {
 			}
 
 
-			if ( !isset( $elementJoin ) ) {
+			if ( ! isset( $elementJoin ) ) {
 				// this might also happen in case element got removed from page after user loaded page, and afterward user triggers request to now deleted element - in this case we don't have a security problem
 				throw new SecurityException( 'Invalid elementPrimary ' . $elementPrimary . ' on url ' . $url->url . ' for page passed, MAY be security relevant.' );
 			}
 
 			$template = new Template( $storage, STROOT . '/template/element.php' );
 
-			$indexedAreas = array( 'element' => array( array( 'element' => $elementJoin->element->load(), 'columns' => $elementJoin->columns, 'columnKey' => $columnKey ) ) );
+			$indexedAreas = array(
+				'element' => array(
+					array(
+						'element'   => $elementJoin->element->load(),
+						'columns'   => $elementJoin->columns,
+						'columnKey' => $columnKey
+					)
+				)
+			);
 
 			$context[ 'isDirectLink' ] = true;
 
@@ -158,11 +187,15 @@ class UHPage implements IURLHandler {
 			foreach ( $areaJoins as $areaJoin ) {
 				$k = $areaJoin->key;
 
-				if ( !isset( $indexedAreas[ $k ] ) ) {
+				if ( ! isset( $indexedAreas[ $k ] ) ) {
 					$indexedAreas[ $k ] = array();
 				}
 
-				$indexedAreas[ $k ][ ] = array( 'element' => $areaJoin->area, 'columns' => $areaJoin->columns, 'columnKey' => $k ); // TODO: more data?
+				$indexedAreas[ $k ][ ] = array(
+					'element'   => $areaJoin->area,
+					'columns'   => $areaJoin->columns,
+					'columnKey' => $k
+				); // TODO: more data?
 			}
 
 			$context[ 'isDirectLink' ] = false;
@@ -175,22 +208,22 @@ class UHPage implements IURLHandler {
 		}
 
 		if ( $output ) {
-			$section = Config::section('web');
+			$section = Config::section( 'web' );
 
-			if (!empty($section['enableDebugParameter'])) {
-				if ( !empty( $_GET[ 'qc' ] ) ) {
+			if ( ! empty( $section[ 'enableDebugParameter' ] ) ) {
+				if ( ! empty( $_GET[ 'qc' ] ) ) {
 					$output .= 'Query count: ' . $storage->getQueryCount() . ' queries<br>';
 				}
 
-				if ( !empty( $_GET[ 'qt' ] ) ) {
+				if ( ! empty( $_GET[ 'qt' ] ) ) {
 					$output .= 'Query time: ' . ( $storage->getQueryTime() * 1000 ) . 'ms<br>';
 				}
 
-				if ( !empty( $_GET[ 'tt' ] ) && isset( $_SERVER[ 'REQUEST_TIME_FLOAT' ] ) ) {
+				if ( ! empty( $_GET[ 'tt' ] ) && isset( $_SERVER[ 'REQUEST_TIME_FLOAT' ] ) ) {
 					$output .= 'Total time: ' . ( ( microtime( true ) - $_SERVER[ 'REQUEST_TIME_FLOAT' ] ) * 1000 ) . 'ms<br>';
 				}
 
-				if ( !empty( $_GET[ 'qbt' ] ) ) {
+				if ( ! empty( $_GET[ 'qbt' ] ) ) {
 					$output .= 'Query build time: ' . ( $storage->getQueryBuildTime() * 1000 ) . 'ms<br>';
 				}
 			}
@@ -215,7 +248,7 @@ class UHPage implements IURLHandler {
 							}, $output);
 						}
 			*/
-			$acceptGzip = false;
+			$acceptGzip   = false;
 			$acceptHeader = $requestInfo->getServerInfo( 'HTTP_ACCEPT_ENCODING' );
 
 			// TODO: cache gzencoded output ?
